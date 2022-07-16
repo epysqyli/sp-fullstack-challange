559822cac1816c3bac8d6918ad7d6984b5847616
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-07-16 21:04:52 +0200

    routes: restore rate limiter

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,11 +1,6 @@
 <?php
 
 use App\Http\Controllers\AirportsController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 
 Route::get('/airports', [AirportsController::class, 'index']);
