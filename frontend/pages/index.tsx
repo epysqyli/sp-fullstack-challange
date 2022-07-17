def0223e36ba03747ce01d10fbdc0f2b2dda0f5f
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-07-17 21:50:04 +0200

    index: add server side props

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,23 @@
+import { AxiosResponse } from "axios";
+import { GetServerSideProps } from "next";
 import { ReactElement } from "react";
+import Airport from "../interfaces/Airport";
 import Layout from "../layouts/Layout";
+import { getAirports } from "../lib/apiCalls";
 import NextPageLayout from "../types/NextPageLayout";
 
-const Index: NextPageLayout = () => {
+export const getServerSideProps: GetServerSideProps = async () => {
+  const resp: AxiosResponse<Array<Airport>> = await getAirports();
+  const airports: Array<Airport> = resp.data;
+
+  return { props: { airports } };
+};
+
+interface Props {
+  airports: Array<Airport>;
+}
+
+const Index: NextPageLayout<Props> = ({ airports }: Props) => {
   return <div></div>;
 };
 
