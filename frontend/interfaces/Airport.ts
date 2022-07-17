86d964871ddabbbeafa2b6f35f1aaf4d09b2b793
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-07-17 21:39:42 +0200

    interfaces: add Airport and Flight interfaces

---
+++ frontend/interfaces/Airport.ts
@@ -0,0 +1,11 @@
+interface Airport {
+  id: number;
+  name: string;
+  code: string;
+  lat: number;
+  lng: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export default Airport;
---
+++ frontend/interfaces/Flight.ts
@@ -0,0 +1,9 @@
+interface Flight {
+  id: number;
+  departure_code: string;
+  arrival_code: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export default Flight;
