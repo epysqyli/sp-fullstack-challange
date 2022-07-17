8245a8fe266ff5c2457d5b0ea73bfb94b72a1987
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-07-17 11:58:13 +0200

    flights: add seeder

--- backend/database/seeders/FlightSeeder.php
+++ backend/database/seeders/FlightSeeder.php
@@ -4,10 +4,41 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Flight;
+use App\Models\Airport;
 
-class AirportSeeder extends Seeder
+class FlightSeeder extends Seeder
 {
+  private $airports_permutations = [];
+
+  private function combine_airports()
+  {
+    $codes = Airport::all()->pluck('code')->toArray();
+    foreach ($codes as $code) {
+      for ($i = 0; $i < count($codes); $i++) {
+        if ($codes[$i] != $code) {
+          array_push($this->airports_permutations, [$code, $codes[$i]]);
+        }
+      }
+    }
+  }
+
+  private function createFlights()
+  {
+    foreach ($this->airports_permutations as $airports) {
+      $flight = new Flight();
+      $flight->departure_code = $airports[0];
+      $flight->arrival_code = $airports[1];
+      $flight->price = $flight->get_price() * rand(75, 125) / 100;
+      $flight->save();
+    }
+  }
+
   public function run()
   {
+    $this->combine_airports();
+
+    // calling createFlights twice to ensure two flights with different price for each airports pair
+    $this->createFlights();
+    $this->createFlights();
   }
 }
