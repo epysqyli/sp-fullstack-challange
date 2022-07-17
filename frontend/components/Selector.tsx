9485b9ae4aaae5d4732720daeafe21991531b444
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-07-17 23:04:08 +0200

    components: create selector

---
+++ frontend/components/Selector.tsx
@@ -0,0 +1,38 @@
+import { ReactElement } from "react";
+import { MapPin } from "react-feather";
+import type Airport from "../interfaces/Airport";
+
+interface Props {
+  departure: Airport | undefined;
+  arrival: Airport | undefined;
+}
+
+const Selector = ({ departure, arrival }: Props): ReactElement => {
+  return (
+    <div className='flex justify-between items-center border-b-2 pb-5 px-1 text-sm'>
+      <div className='w-2/5'>
+        {departure ? (
+          <div>
+            <div>{departure.name}</div>
+            <div className='text-sm text-slate-500'>{departure.code}</div>
+          </div>
+        ) : (
+          <div>no departure airport selected</div>
+        )}
+      </div>
+      <MapPin size={28} strokeWidth={1.5} className='text-slate-700' />
+      <div className='w-2/5 text-right'>
+        {arrival ? (
+          <div>
+            <div>{arrival.name}</div>
+            <div className='text-sm text-slate-500'>{arrival.code}</div>
+          </div>
+        ) : (
+          <div>no departure airport selected</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Selector;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,12 @@
-import { AxiosResponse } from "axios";
-import { GetServerSideProps } from "next";
-import { ReactElement } from "react";
-import AirportElement from "../components/AirportElement";
-import Airport from "../interfaces/Airport";
+import type { AxiosResponse } from "axios";
+import type { GetServerSideProps } from "next";
+import type Airport from "../interfaces/Airport";
+import { ReactElement, useState } from "react";
 import Layout from "../layouts/Layout";
-import { getAirports } from "../lib/apiCalls";
 import NextPageLayout from "../types/NextPageLayout";
+import AirportElement from "../components/AirportElement";
+import Selector from "../components/Selector";
+import { getAirports } from "../lib/apiCalls";
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const resp: AxiosResponse<Array<Airport>> = await getAirports();
@@ -19,17 +20,13 @@ interface Props {
 }
 
 const Index: NextPageLayout<Props> = ({ airports }: Props) => {
+  const [departure, setDeparture] = useState<Airport>();
+  const [arrival, setArrival] = useState<Airport>();
+
   return (
     <div className='w-5/6 mx-auto'>
-      <h1 className='text-4xl text-center mb-10 font-medium text-slate-700'>Flyaway.now</h1>
-
-      <div className='my-5 grid grid-cols-1 md:grid-cols-2 gap-y-3'>
-        {airports.map((airport) => (
-          <div key={airport.id}>
-            <AirportElement airport={airport} />
-          </div>
-        ))}
-      </div>
+      <h1 className='text-4xl text-center mb-14 font-medium text-slate-700'>Flyaway.now</h1>
+      <Selector departure={departure} arrival={arrival} />
     </div>
   );
 };
