6fa829ee8de0afb8e1796cf992dd9fa04c515634
elvis.pysqyli <elvis.pysqyli@gmail.com>
2022-07-18 15:54:46 +0200

    flights: remove index controller action

--- backend/app/Http/Controllers/FlightsController.php
+++ backend/app/Http/Controllers/FlightsController.php
@@ -7,12 +7,6 @@ use App\Models\Flight;
 
 class FlightsController extends Controller
 {
-  public function index()
-  {
-    $flights = Flight::all();
-    return response()->json($flights);
-  }
-
   public function search($departure_code, $arrival_code)
   {
     $results = Flight::search($departure_code, $arrival_code);
